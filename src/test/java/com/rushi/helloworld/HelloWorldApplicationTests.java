823ac981a84d740ac365b63621e290d91d7abce8
Lokeshmovietalks <paspuletireddylokesh@gmail.com>
2025-06-12 13:06:15 +0530

    Initial commit

---
+++ src/test/java/com/rushi/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushi.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
